82f9d6e81fd6ace3b63d87fc1dba640c9222e33c
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2024-07-19 20:41:20 +0100

    Create API level schema for shop endpoint

--- bot/src/wrapper/api_schema.py
+++ bot/src/wrapper/api_schema.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict, TypeAlias
 
 
 class CompanyPostInput(TypedDict):
@@ -19,3 +19,26 @@ class CompanyPostIdOutput(CompanyPostOutput):
 
 class CompanyPatchIdInput(TypedDict):
     company_name: str
+
+
+class ShopItem(TypedDict):
+    item_name: str
+    item_price: float
+    item_quantity: int
+
+
+ShopGetOutput: TypeAlias = list[ShopItem]
+
+ShopIdGetOutput: TypeAlias = ShopItem
+
+
+class ShopBuyInput(TypedDict):
+    user_id: int
+    quantity: int
+
+
+class ShopBuyOutput(TypedDict):
+    user_id: int
+    item_id: int
+    quantity: int
+    new_balance: float
