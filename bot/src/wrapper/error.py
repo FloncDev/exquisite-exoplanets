fed1882df9d3206fb8e37af7069e8898abd3477b
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2024-07-19 22:01:52 +0100

    Add docstring for error

--- bot/src/wrapper/error.py
+++ bot/src/wrapper/error.py
@@ -1,16 +1,22 @@
-class UnknownError(Exception): ...
+class UnknownError(Exception):
+    """A unknown error have occurred."""
 
 
-class NetworkError(ConnectionError): ...
+class NetworkError(ConnectionError):
+    """A network error have occurred."""
 
 
-class UnknownNetworkError(UnknownError, NetworkError): ...
+class UnknownNetworkError(UnknownError, NetworkError):
+    """An unknown network error have occurred."""
 
 
-class AlreadyExistError(NetworkError, FileExistsError): ...
+class AlreadyExistError(NetworkError, FileExistsError):
+    """The error which represent the material the client attempted to create already exist on the server."""
 
 
-class DoNotExistError(NetworkError, FileNotFoundError): ...
+class DoNotExistError(NetworkError, FileNotFoundError):
+    """The error which represent the requested material do not exist on the server."""
 
 
-class UserError(Exception): ...
+class UserError(Exception):
+    """An error cause by the user causing the request failed to proceed."""
