8a62b0b1a4d6fdda0b71d8dc15cd279ef1dc98ca
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2024-07-20 21:52:55 +0100

    Fix conflict

--- bot/src/main.py
+++ bot/src/main.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
 
-import discord
 from discord.ext import commands
 from dotenv import find_dotenv, load_dotenv
 
@@ -10,44 +9,26 @@ load_dotenv(find_dotenv())
 TOKEN = os.environ["DISCORD_TOKEN"]
 TESTING_GUILD_ID = os.getenv("TESTING_GUILD_ID")
 
+client = commands.Bot(debug_guilds=[TESTING_GUILD_ID] if TESTING_GUILD_ID else None)
 
-class DiscordClient(commands.Bot):
-    """Main class for the discord client."""
 
-    def __init__(self, *, intents: discord.Intents, testing_guild_id: int | None = None) -> None:
-        super().__init__("", intents=intents)
-        self.testing_guild_id = testing_guild_id
+@client.listen()
+async def on_ready() -> None:
+    user = client.user
 
-    async def setup_hook(self) -> None:
-        """Ran when setting up the bot, loads cogs."""
-        for filename in os.listdir("./src/cogs"):
-            if filename.endswith(".py"):
-                await self.load_extension(f"cogs.{filename[:-3]}")
+    if user is None:
+        return
 
-        if self.testing_guild_id:
-            guild = discord.Object(self.testing_guild_id)
-            self.tree.copy_global_to(guild=guild)
-            await self.tree.sync(guild=guild)
-
-    async def on_ready(self) -> None:
-        """Ran when bot has logged in."""
-        user = self.user
-
-        if user is None:
-            return
-
-        # TODO: Setup propper logging
-        print(f"Logged in as {user.name}({user.id})")
+    print(f"Logged in as {user.name}({user.id})")
 
 
 async def main() -> None:
-    discord.utils.setup_logging()
+    for filename in os.listdir("./src/cogs"):
+        if filename.endswith(".py") and filename != "__init__.py":
+            client.load_extension(f"src.cogs.{filename[:-3]}")
 
-    bot = DiscordClient(
-        intents=discord.Intents.default(),
-        testing_guild_id=int(TESTING_GUILD_ID) if TESTING_GUILD_ID else None,
-    )
-    await bot.start(TOKEN)
+    await client.start(TOKEN)
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
