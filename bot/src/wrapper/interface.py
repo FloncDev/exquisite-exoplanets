b282e52e5af85ff1fe6a75f7bca8cec0b495170a
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2024-07-20 15:09:41 +0100

    Add more company interface

--- bot/src/wrapper/interface.py
+++ bot/src/wrapper/interface.py
@@ -4,7 +4,7 @@ from ._communication import CompanyRawAPI
 from .schema import Company
 
 if TYPE_CHECKING:
-    from ._api_schema import CompanyGetIdOutput, CompanyPostInput
+    from ._api_schema import CompanyGetIdOutput, CompanyPatchIdInput, CompanyPostInput
 
 
 class BaseAPI:
@@ -22,8 +22,22 @@ class CompanyAPI(BaseAPI):
         """Create an company for the user and return the Company."""
         src: CompanyPostInput = {"company_name": company_name, "owner_id": user_id}
         await CompanyRawAPI.create_company(self.address, self.token, src)
+        return await self.get_company(user_id)
+
+    async def get_company(self, user_id: int) -> Company:
+        """Get the company from user id."""
         out: CompanyGetIdOutput = await CompanyRawAPI.get_company(self.address, self.token, user_id)
         return Company.from_dict(out)
+
+    async def edit_company_name(self, company: Company | int, new_name: str) -> Company:
+        """Edit the company name with the given user id."""
+        if isinstance(company, Company):
+            user_id: int = company.owner_id
+        else:
+            user_id: int = company
+        src: CompanyPatchIdInput = {"company_name": new_name}
+        await CompanyRawAPI.edit_company_name(self.address, self.token, user_id, src)
+        return await self.get_company(user_id)
 
 
 class Interface:
