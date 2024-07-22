8d8a488876ecd84f6542d428b4d6c7893aa6c31d
MilaDog <daniel.ryan.sergeant@gmail.com>
2024-07-22 02:37:55 +0200

    It helps adding the files...

---
+++ api/src/routers/shop.py
@@ -0,0 +1,79 @@
+# pyright: reportUnnecessaryComparison=false
+
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
+from src.classes.company import CompanyRepresentation
+from src.classes.pagination import ShopPagination
+from src.classes.shop import ShopRepresentation
+from src.db import get_session
+from src.models import ShopItemCreate, ShopItemPublic, ShopItemPurchase, ShopItemUpdate
+
+router = APIRouter()
+
+
+@router.get("/shop")
+async def get_shop(params: ShopPagination = Depends(), session: Session = Depends(get_session)) -> dict[str, Any]:
+    """Get the items that are currently available in the shop to purchase.
+
+    :param params: Pagination parameters.
+    :param session: Database session.
+    :return: Fetched shop items
+    """
+    return ShopRepresentation.fetch_shop(session=session, params=params)
+
+
+@router.post("/shop")
+async def create_shop_item(data: ShopItemCreate, session: Session = Depends(get_session)) -> None:
+    """Create a new Item in the Shop.
+
+    :param data: Shop Item data.
+    :param session: Database session.
+    :return: None
+    """
+    ShopRepresentation.create_item(session=session, data=data)
+    raise HTTPException(status_code=200, detail="Shop Item successfully created.")
+
+
+@router.get("/shop/{item_id}")
+async def get_shop_item(item_id: int, session: Session = Depends(get_session)) -> ShopItemPublic | None:
+    """Get the target Shop Item from the database.
+
+    :param item_id: ID of the Shop Item to fetch.
+    :param session: Database session.
+    :return: Fetched Shop Item
+    """
+    if (item := ShopRepresentation.get_item(item_id=item_id, session=session)) is not None:
+        return item.get_details()
+    raise HTTPException(status_code=404, detail="Shop Item not found.")
+
+
+@router.patch("/shop/{item_id}")
+async def update_shop_item(item_id: int, data: ShopItemUpdate, session: Session = Depends(get_session)) -> None:  # noqa: ARG001
+    """Update the target Shop Item with the given Data.
+
+    :param item_id: ID of Shop Item to update.
+    :param data: Data to update.
+    :param session: Database session.
+    :return: None
+    """
+    ShopRepresentation.update_item(session=session, data=data)
+    raise HTTPException(status_code=200, detail="Shop Item successfully updated.")
+
+
+@router.post("/shop/{item_id}/buy")
+async def company_purchase_shop_item(item_id: int, data: ShopItemPurchase, session: Session = Depends(get_session)) -> None:
+    """Target company purchases the target item.
+
+    :param item_id: ID of Shop Item.
+    :param data: Data of the Shop Item being bought.
+    :param session: Database session.
+    :return: None
+    """
+    fetched_company: CompanyRepresentation = CompanyRepresentation.fetch_company(session=session, company_id=data.company_id)
+    target_item: ShopRepresentation.ShopItemRepresentation = ShopRepresentation.get_item(session=session, item_id=item_id)
+
+    if (c := fetched_company.get_company()) is not None:
+        target_item.purchase_item(company=c, data=data)
+    raise HTTPException(status_code=200, detail="Shop Item successfully purchased.")
