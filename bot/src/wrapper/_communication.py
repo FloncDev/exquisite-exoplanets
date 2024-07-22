5490c5cf9293f0c36697adc53d153b878945b357
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2024-07-22 22:30:22 +0100

    Fix missing forward slash

--- bot/src/wrapper/_communication.py
+++ bot/src/wrapper/_communication.py
@@ -221,7 +221,7 @@ class UserRawAPI:
         """Get the User information by user id through direct HTTP request."""
 
         async def caller(session: aiohttp.ClientSession) -> UserIdGetOutput:
-            async with session.get(f"user/{user_id}") as resp:
+            async with session.get(f"/user/{user_id}") as resp:
                 if resp.ok:
                     return await resp.json()
                 if resp.status == Status.NOT_FOUND:
@@ -237,7 +237,7 @@ class UserRawAPI:
         """Create the user by user id through a direct HTTP request."""
 
         async def caller(session: aiohttp.ClientSession) -> None:
-            async with session.post(f"user/{user_id}") as resp:
+            async with session.post(f"/user/{user_id}") as resp:
                 if resp.ok:
                     return
                 if resp.status == Status.CONFLICT:
@@ -255,7 +255,7 @@ class UserRawAPI:
         """Update the user experience by user id through a direct HTTP request."""
 
         async def caller(session: aiohttp.ClientSession) -> UserIdExperiencePatchOutput:
-            async with session.post(f"user/{user_id}/experience", json=src) as resp:
+            async with session.post(f"/user/{user_id}/experience", json=src) as resp:
                 if resp.ok:
                     return await resp.json()
                 if resp.status == Status.NOT_FOUND:
