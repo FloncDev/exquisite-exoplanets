e02b4aaaedffa9b22ee93e44dd25581d420ed309
FloncDev <FloncDev@users.noreply.github.com>
2024-07-24 20:42:04 +0200

    🚨 Fix linting for resource_collector.py

--- api/src/resource_collector.py
+++ api/src/resource_collector.py
@@ -1,10 +1,13 @@
-from Resource import Resource
-from YamlReader import YamlReader
 import logging
+
+from src import YamlReader
 
 collector_logger = logging.getLogger(__name__)
 
+
 class ResourceCollector:
-    __config:dict = YamlReader('ResourceCollector.yaml').contents
-    def __init__(self):
-        ...
+    """Resource Collecter -- Please change to a better description."""
+
+    _config = YamlReader("ResourceCollector.yaml").contents
+
+    def __init__(self) -> None: ...
