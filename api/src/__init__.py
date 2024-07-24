f75be946e8df7364519a996ecc95c2f9b1f30578
FloncDev <FloncDev@users.noreply.github.com>
2024-07-24 18:52:08 +0200

    🧑‍💻 Add classes to __init__.py

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,2 +1,7 @@
-import os
-root = os.path.abspath('../../')
+from .company import Company
+from .planet import Planet
+from .resource import Resource
+from .resourceCollector import ResourceCollector
+from .yamlreader import YamlReader
+
+__all__ = ["Company", "Planet", "Resource", "ResourceCollector", "YamlReader"]
