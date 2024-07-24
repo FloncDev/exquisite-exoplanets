1014f90aebf76a270ab7ca8e70b3c0cb9341517b
FloncDev <FloncDev@users.noreply.github.com>
2024-07-24 19:05:11 +0200

    🚚 Change names in  __init__.py

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,7 +1,7 @@
 from .company import Company
 from .planet import Planet
 from .resource import Resource
-from .resourceCollector import ResourceCollector
-from .yamlReader import YamlReader
+from .resource_collector import ResourceCollector
+from .yaml_reader import YamlReader
 
 __all__ = ["Company", "Planet", "Resource", "ResourceCollector", "YamlReader"]
