9e458ba8d9c361bac822a90381d98e54fac14143
FloncDev <FloncDev@users.noreply.github.com>
2024-07-24 19:04:09 +0200

    🚚 Rename files

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -2,6 +2,6 @@ from .company import Company
 from .planet import Planet
 from .resource import Resource
 from .resourceCollector import ResourceCollector
-from .yamlreader import YamlReader
+from .yamlReader import YamlReader
 
 __all__ = ["Company", "Planet", "Resource", "ResourceCollector", "YamlReader"]
