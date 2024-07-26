030e538308862b6dd935825352c6fbf2210ad770
FloncDev <FloncDev@users.noreply.github.com>
2024-07-26 19:44:32 +0200

    🎨 Move shop into a command group
    + add autocomplete stuff for /shop buy

--- bot/src/cogs/shop.py
+++ bot/src/cogs/shop.py
@@ -1,9 +1,17 @@
 import discord
+from discord.commands import SlashCommandGroup, option
 from discord.ext import commands, pages
 
-from src.context import Context
+from src.context import AutocompleteContext, Context
 from src.main import Client
 from src.wrapper import ShopItem
+
+
+async def item_autocomplete(_: AutocompleteContext) -> list[str]:
+    # TODO: Change onces wrapper is fixed
+    shop = [ShopItem(id=i, name=f"Item {i+1}", price=10, quantity=10) for i in range(10)]
+
+    return [item.name for item in shop]
 
 
 class Shop(commands.Cog):
@@ -12,8 +20,10 @@ class Shop(commands.Cog):
     def __init__(self, client: Client) -> None:
         self.client = client
 
-    @commands.slash_command()
-    async def shop(self, ctx: Context) -> None:
+    shop = SlashCommandGroup("shop")
+
+    @shop.command()
+    async def view(self, ctx: Context) -> None:
         """Interact with the shop."""
         # Temporary while wrapper is getting fixed
         shop = [ShopItem(id=i, name=f"Item {i+1}", price=10, quantity=10) for i in range(10)]
@@ -36,8 +46,21 @@ class Shop(commands.Cog):
                 value=f"${item.price:.2f}\n{item.quantity} in stock",
             )
 
-        paginator = pages.Paginator(pages=shop_pages)  # pyright: ignore[reportArgumentType]
+        paginator = pages.Paginator(
+            pages=shop_pages  # pyright: ignore[reportArgumentType]
+        )
         await paginator.respond(ctx.interaction)
+
+    @shop.command()
+    @option(
+        "item",
+        str,
+        description="The item you want to buy.",
+        autocomplete=item_autocomplete,
+    )
+    @option("quantity", int, description="How many you want to buy, defaults to 1.")
+    async def buy(self, ctx: Context, item: str, quantity: int = 1) -> None:
+        """Buy an item in the shop."""
 
 
 def setup(client: Client) -> None:
