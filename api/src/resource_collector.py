a7717a4ac784930bbe41eab7cc45514e9a264ef4
Julien Crambes <71984039+Elektriman@users.noreply.github.com>
2024-07-28 11:03:10 +0200

    add an auto stopping feature for the resource collectors

--- api/src/resource_collector.py
+++ api/src/resource_collector.py
@@ -7,44 +7,49 @@ collector_logger = logging.getLogger(__name__)
 
 
 class ResourceCollector:
-    """Class that represents any upgradable machine that can harvest materials"""
+    """Class that represents any upgradable machine that can harvest materials
+    Each instance is bound to one material only"""
 
     config = YamlReader("ResourceCollector.yaml").contents
-    epoch_definition = datetime.timedelta(hours=1)
+    epoch_definition = datetime.timedelta(microseconds=1)
 
     def __init__(self,
                  collector_id: str,
-                 resource_id: str,
+                 resource: Resource,
                  tier: int = 0) -> None:
         collector_conf = self.config[collector_id]
         self.name = collector_conf["name"]
         self.tier = tier
-        self.init_price = collector_conf["init_price"]
-        self.init_speed = collector_conf["init_speed"]
-        self.speed_upgrade_upscale = collector_conf["speed_upgrade_upscale"]
-        self.speed_upgrade_init_price = collector_conf["speed_upgrade_init_price"]
-        self.speed_upgrade_price_upscale = collector_conf["speed_upgrade_price_upscale"]
-        self.cost_of_use = collector_conf["cost_of_use"]
-        self.cost_of_use_price_upscale = collector_conf["cost_of_use_price_upscale"]
-        self.resources = collector_conf["resources"]
-        if resource_id not in self.resources.keys():
-            raise ValueError(f"Cannot extract {Resource.config[resource_id]['name']} with {self.name}")
-        elif self.resources[resource_id] > tier:
-            raise ValueError(f"You need this extractor to be level {self.resources[resource_id]} "
-                             f"in order to extract {Resource.config[resource_id]['name']}")
+        self.auto_stop = float('inf')
+        self.__init_price = collector_conf["init_price"]
+        self.__init_speed = collector_conf["init_speed"]
+        self.__upgrade_upscale = collector_conf["upgrade_upscale"]
+        self.__upgrade_init_price = collector_conf["upgrade_init_price"]
+        self.__upgrade_price_upscale = collector_conf["upgrade_price_upscale"]
+        self.__cost_of_use = collector_conf["cost_of_use"]
+        self.__cost_of_use_price_upscale = collector_conf["cost_of_use_price_upscale"]
+        resources = collector_conf["resources"]
+        if resource.r_id not in resources.keys():
+            raise ValueError(f"Cannot extract {resource.name} with {self.name}")
+        elif resource.tier > self.tier:
+            raise ValueError(f"You need this extractor to be level {resource.tier}"
+                             f"in order to extract {resource.name}")
         else:
-            self.assignment = Resource(resource_id, tier)
-        self.started_at: datetime.datetime = None
-        self.last_collected_at: datetime.datetime = None
-        self.epochs = 0
+            self.resource = resource
+        self.__started_at: datetime.datetime = None
+        self.__last_collected_at: datetime.datetime = None
+        self.__epochs = 0
+
+    def __get_relative_tier(self):
+        return self.tier - self.resource.tier
 
     def get_speed(self) -> float:
         """returns the harvesting speed"""
-        return self.init_speed * self.speed_upgrade_upscale ** self.tier
+        return self.__init_speed * self.__upgrade_upscale ** self.__get_relative_tier()
 
-    def get_next_speed_upgrade_cost(self) -> float:
+    def get_next_upgrade_cost(self) -> float:
         """returns the next harvesting speed upgrade price"""
-        return self.speed_upgrade_init_price * self.speed_upgrade_price_upscale ** self.tier
+        return self.__upgrade_init_price * self.__upgrade_price_upscale ** self.__get_relative_tier()
 
     def upgrade(self) -> None:
         """makes the collector upgrade 1 tier"""
@@ -52,28 +57,34 @@ class ResourceCollector:
 
     def get_cost(self, n: int = 1) -> float:
         """returns the cost of use for `n` usage"""
-        return (self.cost_of_use * self.cost_of_use_price_upscale ** self.tier) * n
+        return (self.__cost_of_use * self.__cost_of_use_price_upscale ** self.__get_relative_tier()) * n
 
     def start(self) -> None:
         """starts the resource harvesting"""
         start_time = datetime.datetime.now()
-        self.started_at = start_time
-        self.last_collected_at = start_time
+        self.__started_at = start_time
+        self.__last_collected_at = start_time
 
     def stop(self) -> None:
         """Stops the resource harvesting"""
-        self.started_at = None
+        self.__started_at = None
 
-    def collect(self) -> tuple[float,float]:
+    def set_auto_stop(self,
+                      stop_value: float) -> None:
+        """setter for the auto stop parameter"""
+        if stop_value < 1:
+            raise ValueError(f"auto stopping has to be greater or equal to 1")
+        self.auto_stop = stop_value
+
+    def collect(self) -> tuple[float, float, float]:
         """collects resources then returns the amount collected and the cost of harvesting"""
-        if not self.started_at:
+        if not self.__last_collected_at:
             raise ValueError("collector has to be started before collecting")
         collection_time = datetime.datetime.now()
-        epochs_since_last_collection = self.get_speed() * (collection_time - self.last_collected_at) // self.epoch_definition
-        self.epochs += epochs_since_last_collection
-        units_collected = self.assignment.collect(epochs_since_last_collection)
-        return units_collected, self.get_cost(epochs_since_last_collection)
+        epochs_since_last_collection = self.get_speed() * (
+                    collection_time - self.__last_collected_at) // self.epoch_definition
+        epochs_since_last_collection = int(min(epochs_since_last_collection, self.auto_stop))
+        self.__epochs += epochs_since_last_collection
+        units_collected = self.resource.collect(epochs_since_last_collection)
+        return units_collected, self.get_cost(epochs_since_last_collection), self.resource.unit_price * units_collected
 
-
-if __name__ == "__main__":
-    ResourceCollector("MI00",  "UR00", 2)
