b49abf106b57e4bf8b360e2c6f9abf89546cfca3
FloncDev <FloncDev@users.noreply.github.com>
2024-07-28 13:01:49 +0200

    🚧 Add boilerplate for explore and planet cogs

---
+++ bot/src/cogs/explore.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+from src.context import Context
+from src.main import Client
+
+
+class Explore(commands.Cog):
+    """Cog containing commands related to exploring."""
+
+    def __init__(self, client: Client) -> None:
+        self.client = client
+
+    @commands.slash_command()
+    async def planet(self, ctx: Context) -> None:
+        """Ping the bot."""
+        await ctx.respond("pong")
+
+
+def setup(client: Client) -> None:
+    client.add_cog(Explore(client))
